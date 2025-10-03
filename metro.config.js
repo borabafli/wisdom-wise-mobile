c8cd43bcc0cac1869254e75286e6cc8813f4483b
Nikolai <nikolai.tschuler@gmail.com>
2025-10-04 00:11:29 +0200

    Disabled new architecture

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,3 @@
-// Polyfill for Node.js compatibility issue
-const os = require('os');
-if (!os.availableParallelism) {
-  os.availableParallelism = () => os.cpus().length;
-}
-
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
